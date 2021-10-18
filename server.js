daef2cee7e3a46716a654cd893a75ea6987b0a16
datacreator001 <amirrorphone@gmail.com>
2021-10-17 22:26:15 -0400

    Finish with movie routes

--- server.js
+++ server.js
@@ -1,41 +1,56 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-const seed = require('./seed')
-seed()
+const seed = require('./seed');
+seed();
+
+const { Movie } = require('./models/Index');
+const { User } = require('./models/Index');
+
+app.use(express.json());
 
 
-const{Movie}= require('./models/Index')
-const{User}= require('./models/Index')
 
+app.get('/', async (req, res) => {
+	res.redirect('/movies');
+});
 
+app.get('/movies', async (req, res) => {
+	let allMovies = await Movie.findAll();
+	res.json({ allMovies });
+});
 
-app.use(express.json())
-
-app.get('/users', async(req,res)=>{
-    let allUsers = await User.findAll()
-    res.json({allUsers})
-})
-
-app.get('/',async(req, res)=>{
-    res.redirect('/movies')
-})
-
-app.get('/movies',async (req,res)=>{
-    let allMovies = await Movie.findAll()
-    res.json({allMovies})
-})
-
-app.get('/movie/:id', async (req,res)=>{
-    let id = req.params.id;
-    let movie = await Movie.findByPk(id);
-    res.json({movie})
-})
+app.get('/movie/:id', async (req, res) => {
+	let id = req.params.id;
+	let movie = await Movie.findByPk(id);
+	res.json({ movie });
+});
 app.post('/movie', async (req, res) => {
-	await Movie.create(req.body).then((movie)=>{
-        res.status(201).json(movie)
-    });
+	await Movie.create(req.body).then((movie) => {
+		res.status(201).json(movie);
+	});
 	res.send(`New Movie added`);
+});
+
+app.put('/movie/:id', async (req, res) => {
+	let id = req.params.id; 
+    
+	let movieToUpdate = await Movie.findByPk(id); 
+	await movieToUpdate.update(req.body);
+	res.send(`Movie has been updated`);
+});
+
+app.delete('/movie/:id', async (req, res) => {
+	//where helps us filter which column we are trying to identify
+	await Movie.destroy({
+		where: { id: req.params.id },
+	});
+	res.send(`movie deleted`);
+});
+
+app.get('/users', async (req, res) => {
+	let allUsers = await User.findAll();
+	res.json({ allUsers });
 });
 
 app.post('/user', async (req, res) => {
@@ -45,26 +60,7 @@ app.post('/user', async (req, res) => {
 	res.send(`New User added`);
 });
 
-app.put('/movie/:id', async (req, res) => {
-	let id = req.params.id; //10
-	
-    Movie.update(id,)
-    
-    //find the row
-	// let movieToUpdate = await movie.findByPk(id); // {id: 10 'name': 'nameOfSong', 'genre': 'rock'}
-	// await movieToUpdate.update(req.body);
-	res.send(`song has been updated`);
-});
-
-app.delete('/movie/:id', async (req, res) => {
-	//where helps us filter which column we are trying to identify
-	await movie.destroy({
-		where: { id: req.params.id },
-	});
-	res.send(`song deleted`);
-});
-
-app.get('/movies/:userId', async (req, res) => {
+app.get('/userMovies/:userId', async (req, res) => {
 	const { userId } = req.params;
 
 	const show = await Movie.findAll({
@@ -81,7 +77,6 @@ app.get('/movies/:userId', async (req, res) => {
 	res.json(show);
 });
 
-
-app.listen(4000,()=>{
-    console.log('app is listening on port 4000 🎥');
-})
+app.listen(4000, () => {
+	console.log('app is listening on port 4000 🎥');
+});
